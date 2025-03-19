b2122f2618ce04146708a7f82b07026d3e84f1c9
SputnikPro <averina@sputnik.llc>
2025-03-19 17:56:59 +0300

    fixx name

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
-import {resolve} from "path";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: '/emanufacturer/', 
-  build: {
+	base: '/ES/',
+	build: {
 		outDir: 'dist',
 		rollupOptions: {
 			input: {
